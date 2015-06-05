d76faa7de034cfdf347debe1e4057b2be812a090
Patent2net <patent2net@gmail.com>
2015-06-05 09:50:29 +0200

    CollectedEtTraite update
    Changin order of several program to optimize analyse of results

--- Development/CollectETraite.bat
+++ Development/CollectETraite.bat
@@ -1,8 +1,24 @@
 cd Patent2Net
 OPSGatherPatentsv2.exe
-OPSGatherAugment-Families.exe
-OPSGatherContentsV1-Iramuteq.exe
-P2N-Authors.exe 
+CartographyCountry.exe
+FormateExport.exe
+FusionIramuteq2.exe
 P2N-Applicants.exe
-P2N-ApplicantsCrossTech.exe  
+P2N-ApplicantsCrossTech.exe
+P2N-Authors.exe
+P2N-AuthorsApplicants.exe
+P2N-CountryCrossTech.exe
+P2N-Families.exe
+P2N-FamiliesHierarc.exe
+P2N-InventorCrossTech.exe
+P2N-CrossTech.exe
+P2N-V5.exe 
+OPSGatherAugment-Families.exe
+FormateExportFamilies.exe
+Interface2.exe
 cd ..
+rem OPSGatherContentsV1-Iramuteq.exe
+REM OPSGatherAbstracts-Iramuteq.exe
+rem Cleaning.exe
+
+REM FusionIramuteq.exe

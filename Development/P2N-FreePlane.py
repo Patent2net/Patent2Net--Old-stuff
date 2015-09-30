2da303b090071aaa2c7cf7796033fb4711066aa8
cvanderlei <celsovanderlei@gmail.com>
2015-09-30 10:17:21 -0300

    IPC 4
    Added the IPC4 level to the mind map.

--- Development/P2N-FreePlane.py
+++ Development/P2N-FreePlane.py
@@ -7,7 +7,8 @@ Created on Mon Aug 24 15:34:15 2015
 Objective: Crate a FreePlane (mindmap) file
 """
 import pickle
-from P2N_FreePlaneLib import LoadDescs, nodecolor, Ipc1Text, Ipc3Text, CalcSizeIpc1
+from P2N_FreePlaneLib import LoadDescs, nodecolor, Ipc1Text, CalcSizeIpc1
+from P2N_FreePlaneLib import Ipc3Text, CalcSizeIpc3, Ipc4Text,CalcSizeIpc4
 
 DataBrevets1 = []
 DataBrevets3 = []
@@ -50,6 +51,7 @@ try:
     with open(ResultPathBiblio+'//'+ndf, 'r') as fic:
         DataBrevets1 = pickle.load(fic)
         DataBrevets3 = DataBrevets1
+        DataBrevets4 = DataBrevets1
         DataBrevets7 = DataBrevets1
         BrevetsTotal = str(len(DataBrevets1['brevets']))
 except:
@@ -100,8 +102,8 @@ for i in DataBrevets1['brevets']:
                 else:
                     nodeside = 'right'
                 ncolor = nodecolor(ncolor)
-                nsize, ncount = CalcSizeIpc1(nIpc1,DataBrevets1)
-                nodetext = Ipc1Text(nIpc1) + " (" + ncount + ")"
+                nsize, ncount1 = CalcSizeIpc1(nIpc1,DataBrevets1)
+                nodetext = Ipc1Text(nIpc1) + " (" + ncount1 + ")"
                 fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''"  BACKGROUND_COLOR="''' + ncolor + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
                 fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
                 fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
@@ -113,21 +115,75 @@ for i in DataBrevets1['brevets']:
 # Node level IPC3                               
                                 nIpc3 = l
                                 ListIpc3.append(nIpc3)
-                                nodetext = Ipc3Text(nIpc3)
-                                nsize = '10'
-                                fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
+                                nsize, ncount3 = CalcSizeIpc3(nIpc3,DataBrevets3,ncount1)
+                                nodetext = Ipc3Text(nIpc3) + " (" + ncount3 + ")"
+                                fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#F9F4F4" STYLE="bubble" MAX_WIDTH="300">\n''')
                                 fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
                                 fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+### BEFORE
+                                ListIpc4 = []
+                                for m in DataBrevets4['brevets']:
+                                    if type(m['IPCR4']) == list:
+                                        for n in m['IPCR4']:
+                                            if ListIpc4.count(n) == 0 and n !='' and n.count(nIpc3,0,3) == 1:
+                # Node level IPC4                               
+                                                nIpc4 = n
+                                                ListIpc4.append(nIpc4)
+                                                nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                                nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                                fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                                fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                                fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                                fictemp.write('''</node> \n''')
+                                    else:
+                                        if ListIpc4.count(m['IPCR4']) == 0 and m['IPCR4'] != '' and m['IPCR4'].count(nIpc3,0,3) == 1:
+                                            nIpc4 = m['IPCR4']
+                                            ListIpc4.append(nIpc4)
+                                            nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                            nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                            fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                            fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                            fictemp.write('''</node> \n''')
+                # End node level IPC4
+### AFTER
                                 fictemp.write('''</node> \n''')
                     else:
                         if ListIpc3.count(k['IPCR3']) == 0 and k['IPCR3'] != '' and k['IPCR3'].count(nIpc1,0,1) == 1:
                             nIpc3 = k['IPCR3']
                             ListIpc3.append(nIpc3)
-                            nodetext = Ipc3Text(nIpc3)
-                            nsize = '10'
-                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
+                            nsize, ncount3 = CalcSizeIpc3(nIpc3,DataBrevets3,ncount1)
+                            nodetext = Ipc3Text(nIpc3) + " (" + ncount3 + ")"
+                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#F9F4F4" STYLE="bubble" MAX_WIDTH="300">\n''')
                             fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
                             fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+### BEFORE
+                            ListIpc4 = []
+                            for m in DataBrevets4['brevets']:
+                                if type(m['IPCR4']) == list:
+                                    for n in m['IPCR4']:
+                                        if ListIpc4.count(n) == 0 and n !='' and n.count(nIpc3,0,3) == 1:
+            # Node level IPC4                               
+                                            nIpc4 = n
+                                            ListIpc4.append(nIpc4)
+                                            nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                            nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                            fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                            fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                            fictemp.write('''</node> \n''')
+                                else:
+                                    if ListIpc4.count(m['IPCR4']) == 0 and m['IPCR4'] != '' and m['IPCR4'].count(nIpc3,0,3) == 1:
+                                        nIpc4 = m['IPCR4']
+                                        ListIpc4.append(nIpc4)
+                                        nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                        nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                        fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                        fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                        fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                        fictemp.write('''</node> \n''')
+            # End node level IPC4
+### AFTER                            
                             fictemp.write('''</node> \n''')
 # End node level IPC3
                 fictemp.write('''</node> \n''')
@@ -142,8 +198,8 @@ for i in DataBrevets1['brevets']:
             else:
                 nodeside = 'right'
             ncolor = nodecolor(ncolor)
-            nsize, ncount  = CalcSizeIpc1(nIpc1,DataBrevets1)
-            nodetext = Ipc1Text(nIpc1) + " (" + ncount + ")"
+            nsize, ncount1  = CalcSizeIpc1(nIpc1,DataBrevets1)
+            nodetext = Ipc1Text(nIpc1) + " (" + ncount1 + ")"
             fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="''' + ncolor + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
             fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
             fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
@@ -155,21 +211,75 @@ for i in DataBrevets1['brevets']:
 # Node level IPC3
                             nIpc3 = j
                             ListIpc3.append(nIpc3)
-                            nodetext = Ipc3Text(nIpc3)
-                            nsize = '10'
-                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
+                            nsize, ncount3 = CalcSizeIpc3(nIpc3,DataBrevets3,ncount1)
+                            nodetext = Ipc3Text(nIpc3) + " (" + ncount3 + ")"
+                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#F9F4F4" STYLE="bubble" MAX_WIDTH="300"> \n''')
                             fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
                             fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+### BEFORE
+                            ListIpc4 = []
+                            for m in DataBrevets4['brevets']:
+                                if type(m['IPCR4']) == list:
+                                    for n in m['IPCR4']:
+                                        if ListIpc4.count(n) == 0 and n !='' and n.count(nIpc3,0,3) == 1:
+            # Node level IPC4                               
+                                            nIpc4 = n
+                                            ListIpc4.append(nIpc4)
+                                            nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                            nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                            fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                            fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                            fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                            fictemp.write('''</node> \n''')
+                                else:
+                                    if ListIpc4.count(m['IPCR4']) == 0 and m['IPCR4'] != '' and m['IPCR4'].count(nIpc3,0,3) == 1:
+                                        nIpc4 = m['IPCR4']
+                                        ListIpc4.append(nIpc4)
+                                        nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                        nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                        fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                        fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                        fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                        fictemp.write('''</node> \n''')
+            # End node level IPC4
+### AFTER                            
                             fictemp.write('''</node> \n''')
                 else:
                     if ListIpc3.count(i['IPCR3']) == 0 and i['IPCR3'] != '' and i['IPCR3'].count(nIpc1,0,1) == 1:
                         nIpc3 = i['IPCR3']
                         ListIpc3.append(nIpc3)
-                        nodetext = Ipc3Text(nIpc3)
-                        nsize = '10'
-                        fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" STYLE="bubble" MAX_WIDTH="200"> \n''')
+                        nsize, ncount3 = CalcSizeIpc3(nIpc3,DataBrevets3,ncount1)
+                        nodetext = Ipc3Text(nIpc3) + " (" + ncount3 + ")"
+                        fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#F9F4F4" STYLE="bubble" MAX_WIDTH="300"> \n''')
                         fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
                         fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+### BEFORE
+                        ListIpc4 = []
+                        for m in DataBrevets4['brevets']:
+                            if type(m['IPCR4']) == list:
+                                for n in m['IPCR4']:
+                                    if ListIpc4.count(n) == 0 and n !='' and n.count(nIpc3,0,3) == 1:
+        # Node level IPC4                               
+                                        nIpc4 = n
+                                        ListIpc4.append(nIpc4)
+                                        nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                        nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                        fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                        fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                        fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                        fictemp.write('''</node> \n''')
+                            else:
+                                if ListIpc4.count(m['IPCR4']) == 0 and m['IPCR4'] != '' and m['IPCR4'].count(nIpc3,0,3) == 1:
+                                    nIpc4 = m['IPCR4']
+                                    ListIpc4.append(nIpc4)
+                                    nsize, ncount4 = CalcSizeIpc4(nIpc4,DataBrevets4,ncount3)
+                                    nodetext = Ipc4Text(nIpc4) + " (" + ncount4 + ")"
+                                    fictemp.write('''<node TEXT="'''+ nodetext + '''" POSITION="''' + nodeside + '''" BACKGROUND_COLOR="#FFFFFF" STYLE="bubble" MAX_WIDTH="300">\n''')
+                                    fictemp.write('''<font SIZE="'''+ nsize + '''"/> \n''')
+                                    fictemp.write('''<edge COLOR="''' + ecolor + '''"/> \n''')
+                                    fictemp.write('''</node> \n''')
+        # End node level IPC4
+### AFTER                            
                         fictemp.write('''</node> \n''')
 # End node level IPC3
             fictemp.write('''</node> \n''')
--- Development/P2N_FreePlaneLib.py
+++ Development/P2N_FreePlaneLib.py
@@ -40,6 +40,49 @@ def CalcSizeIpc1(ipcName, ipcList):
     return ntsize, str(int(ipc1count))
 # End CalcSizeIpc1 
 
+def CalcSizeIpc3(ipcName, ipcList, ipc1total):
+    ipc1total = float(ipc1total)
+    ipc3count = float(0)
+    ipc3weight = float(0)
+    for i in ipcList['brevets']:
+#        if type(i['IPCR1']) == list:
+            ipc3count = ipc3count + i['IPCR3'].count(ipcName)
+    ipc3weight = (ipc3count / ipc1total) * 100
+    if ipc3weight > 50:
+        ntsize = '12'
+    elif ipc3weight > 25:
+        ntsize = '10'
+    elif ipc3weight > 15:
+        ntsize = '8'
+    elif ipc3weight >= 10:
+        ntsize = '8'
+    else:
+      ntsize = '8'
+  
+    return ntsize, str(int(ipc3count))
+# End CalcSizeIpc3 
+
+def CalcSizeIpc4(ipcName, ipcList, ipc3total):
+    ipc3total = float(ipc3total)
+    ipc4count = float(0)
+    ipc4weight = float(0)
+    for i in ipcList['brevets']:
+#        if type(i['IPCR1']) == list:
+            ipc4count = ipc4count + i['IPCR4'].count(ipcName)
+    ipc4weight = (ipc4count / ipc3total) * 100
+    if ipc4weight > 50:
+        ntsize = '12'
+    elif ipc4weight > 25:
+        ntsize = '10'
+    elif ipc4weight > 15:
+        ntsize = '8'
+    elif ipc4weight >= 10:
+        ntsize = '8'
+    else:
+      ntsize = '8'
+  
+    return ntsize, str(int(ipc4count))
+# End CalcSizeIpc4 
 
 def LoadDescs():
 
@@ -181,4 +224,74 @@ def Ipc3Text(ipc3):
     return ipc3            
 # end Ipc3Text
     
+def Ipc4Text(ipc4):
+
+    global listdescA
+    global listdescB
+    global listdescC
+    global listdescD
+    global listdescE
+    global listdescF
+    global listdescG
+    global listdescH
+    
+    if ipc4.count('A',0,1) == 1:
+        for lines in listdescA:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('B',0,1) == 1:
+        for lines in listdescB:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('C',0,1) == 1:
+        for lines in listdescC:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('D',0,1) == 1:
+        for lines in listdescD:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('E',0,1) == 1:
+        for lines in listdescE:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('F',0,1) == 1:
+        for lines in listdescF:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('G',0,1) == 1:
+        for lines in listdescG:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+    elif ipc4.count('H',0,1) == 1:
+        for lines in listdescH:
+            try:
+                if lines.count(ipc4+";")>0:
+                    return  ipc4+": "+lines.split(';')[1].strip()
+            except:
+                pass
+
+    return ipc4            
+# end Ipc4Text
 
